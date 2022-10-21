a0d34a8d57207fdac7e9d7eb1e5c3e7e14d0b2ba
Yuri Shemetov <yuri-sh@tut.by>
2022-10-21 11:11:21 +0300

    Add a BNB-bank

--- src/bot_app/mail.py
+++ src/bot_app/mail.py
@@ -203,8 +203,55 @@ def get_new_email(price, servername="imap.yandex.ru"):
                     else:
                         continue
             elif (
+                subject == "SMS-Extra: [BNB-BANK] -> [375292929301]"
+            ):  # <--- replace the text, SMS from BNB-BANK
+                # Body details
+                for part in email_message.walk():
+                    if (
+                        part.get_content_type() == "text/plain"
+                        or part.get_content_type() == "text/html"
+                    ):
+                        body = part.get_payload(decode=True)
+                        
+                        if not path.exists("message/"):
+                            mkdir(f"message/")
+
+                        file_name = "message/" + f"{date_message}.txt"
+                        output_file = open(file_name, "w", encoding="utf-8")
+                        output_file.write(
+                            "From: %s\nTo: %s\nDate: %s\nSubject: %s\n\nBody: \n\n%s"
+                            % (
+                                email_from,
+                                email_to,
+                                local_message_date,
+                                subject,
+                                body.decode("utf-8"),
+                            )
+                        )
+                        output_file.close()
+                        try:
+                            with io.open(
+                                file_name, mode="r", encoding="utf-8"
+                            ) as f_obj:
+                                contents = f_obj.read()
+                                words = contents.split()
+                                i = 0
+                                for word in words:
+                                    i += 1
+                                    if (
+                                        word == "Zachislen"
+                                    ):  # <--- replace the text, BNB-BANK
+                                        money = words[i+1]
+                                        return money
+                                return money
+
+                        except:
+                            return money
+                    else:
+                        continue
+            elif (
                 subject == "SMS-Extra: [MTBANK] -> [375292929301]"
-            ):  # <--- replace the text, SMS from Technobank
+            ):  # <--- replace the text, SMS from MTBANK
                 # Body details
                 for part in email_message.walk():
                     if (
