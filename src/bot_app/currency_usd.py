c190257174f831f422c15c3056e7dd389cd85b6b
Yuri Shemetov <yuri-sh@tut.by>
2023-02-23 20:55:03 +0300

    add the second url for currency_usd

--- src/bot_app/currency_usd.py
+++ src/bot_app/currency_usd.py
@@ -4,12 +4,21 @@ DOLLAR_BTC = "https://blockchain.com/ru/ticker"
 
 def currency_rate():
     response = requests.get(DOLLAR_BTC)
-    try:
-        response.raise_for_status()
-        res = requests.get(DOLLAR_BTC)
-        usd = res.json().get("USD").get("last")
-    except:
-        usd = ""
+    if response.status_code == 200:
+        try:
+            response.raise_for_status()
+            res = requests.get(DOLLAR_BTC)
+            usd = res.json().get("USD").get("last")
+        except:
+            usd = ""
+    else:
+        response = requests.get("https://whattomine.com/asic.json")
+        try:
+            response.raise_for_status()
+            res = requests.get("https://whattomine.com/asic.json")
+            usd = res.json()["coins"]["Bitcoin"]["exchange_rate"]
+        except:
+            usd = ""
     return usd
 
 # DOLLAR_BTC = "https://whattomine.com/asic.json"
