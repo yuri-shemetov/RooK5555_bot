427592aedc8cd0a5f82e90d66e80284774e20a01
Yuri Shemetov <yuri-sh@tut.by>
2023-10-08 19:14:15 +0300

    add paritetbank and alfa-bank for mail

--- src/bot_app/mail.py
+++ src/bot_app/mail.py
@@ -200,6 +200,100 @@ def get_new_email(price, servername="imap.yandex.ru"):
                             return money
                     else:
                         continue
+            elif (
+                subject == "SMS-Extra: [Paritetbank] -> [375333896357]"
+            ):  # <--- replace the text, SMS from Paritetbank
+                # Body details
+                for part in email_message.walk():
+                    if (
+                        part.get_content_type() == "text/plain"
+                        or part.get_content_type() == "text/html"
+                    ):
+                        body = part.get_payload(decode=True)
+                        
+                        if not path.exists("message/"):
+                            mkdir(f"message/")
+
+                        file_name = "message/" + f"{date_message}.txt"
+                        output_file = open(file_name, "w", encoding="utf-8")
+                        output_file.write(
+                            "From: %s\nTo: %s\nDate: %s\nSubject: %s\n\nBody: \n\n%s"
+                            % (
+                                email_from,
+                                email_to,
+                                local_message_date,
+                                subject,
+                                body.decode("utf-8"),
+                            )
+                        )
+                        output_file.close()
+                        try:
+                            with io.open(
+                                file_name, mode="r", encoding="utf-8"
+                            ) as f_obj:
+                                contents = f_obj.read()
+                                words = contents.split()
+                                i = 0
+                                for word in words:
+                                    i += 1
+                                    if (
+                                        word == "POPOLNENIE"
+                                    ):  # <--- replace the text, Paritetbank
+                                        money = words[i]
+                                        return money
+                                return money
+
+                        except:
+                            return money
+                    else:
+                        continue
+            elif (
+                subject == "SMS-Extra: [Alfa-Bank] -> [375333896357]"
+            ):  # <--- replace the text, SMS from Alfa-Bank
+                # Body details
+                for part in email_message.walk():
+                    if (
+                        part.get_content_type() == "text/plain"
+                        or part.get_content_type() == "text/html"
+                    ):
+                        body = part.get_payload(decode=True)
+                        
+                        if not path.exists("message/"):
+                            mkdir(f"message/")
+
+                        file_name = "message/" + f"{date_message}.txt"
+                        output_file = open(file_name, "w", encoding="utf-8")
+                        output_file.write(
+                            "From: %s\nTo: %s\nDate: %s\nSubject: %s\n\nBody: \n\n%s"
+                            % (
+                                email_from,
+                                email_to,
+                                local_message_date,
+                                subject,
+                                body.decode("utf-8"),
+                            )
+                        )
+                        output_file.close()
+                        try:
+                            with io.open(
+                                file_name, mode="r", encoding="utf-8"
+                            ) as f_obj:
+                                contents = f_obj.read()
+                                words = contents.split()
+                                i = 0
+                                for word in words:
+                                    i += 1
+                                    if (
+                                        word == "Postuplenie"
+                                    ):  # <--- replace the text, Alfa-Bank
+                                        money = words[i+1].replace("Summa:", "")
+                                        return money
+                                return money
+
+                        except:
+                            return money
+                    else:
+                        continue
             elif subject == "SMS-Extra: [BNB-BANK] -> [375333896357]":  # <--- replace the text, SMS from BNB-BANK
                 # Body details
                 for part in email_message.walk():
