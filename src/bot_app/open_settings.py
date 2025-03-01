d745ee2d945a7f54acea44ea898219f925d071d0
Yuri <yuri-sh@tut.by>
2025-03-02 00:20:21 +0300

    Add settings_admin_crypto

--- src/bot_app/open_settings.py
+++ src/bot_app/open_settings.py
@@ -1,45 +1,41 @@
 from decimal import *
 
+from bot_app.app import db_settings
+
 # Current bitcoin rate, commission (fees) and percentage
 
 
-def rate_for_one_bit():
-    with open("bot_app/admin/settings/currency_rate.txt", "r") as file_rate:
-        now_rate = file_rate.read()
-        CURRENCY_RATE_FOR_ONE_BITCON = Decimal(now_rate)
+def rate_for_one_bit(name="settings_for_byn"):
+    now_rate = db_settings.get_min_rate(name)[0]
+    CURRENCY_RATE_FOR_ONE_BITCON = Decimal(now_rate)
     return CURRENCY_RATE_FOR_ONE_BITCON
 
 
-def fees():
-    with open("bot_app/admin/settings/fees.txt", "r") as file_fees:
-        now_fees = file_fees.read()
-        FEES = Decimal(now_fees)
+def fees(name="settings_for_byn"):
+    now_fees = db_settings.get_fees(name)[0]
+    FEES = Decimal(now_fees)
     return FEES
 
 
-def percent():
-    with open("bot_app/admin/settings/percent.txt", "r") as file_percent:
-        now_percent = file_percent.read()
-        PERCENT = Decimal(now_percent)
+def percent(name="settings_for_byn"):
+    now_percent = db_settings.get_percent(name)[0]
+    PERCENT = Decimal(now_percent)
     return PERCENT
 
 
-def byn():
-    with open("bot_app/admin/settings/byn.txt", "r") as file_byn:
-        now_byn = file_byn.read()
-        BYN = Decimal(now_byn)
+def byn(name="settings_for_byn"):
+    now_byn = db_settings.get_one_usd_rate(name)[0]
+    BYN = Decimal(now_byn)
     return BYN
 
 
-def min_amount():
-    with open("bot_app/admin/settings/min_amount.txt", "r") as file_min:
-        min_amount = file_min.read()
-        MIN = Decimal(min_amount)
+def min_amount(name="settings_for_byn"):
+    min_amount = db_settings.get_min_amount(name)[0]
+    MIN = Decimal(min_amount)
     return MIN
 
 
-def max_amount():
-    with open("bot_app/admin/settings/max_amount.txt", "r") as file_max:
-        max_amount = file_max.read()
-        MAX = Decimal(max_amount)
+def max_amount(name="settings_for_byn"):
+    max_amount = db_settings.get_max_amount(name)[0]
+    MAX = Decimal(max_amount)
     return MAX
